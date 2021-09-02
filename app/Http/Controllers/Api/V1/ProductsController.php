3192d702192878a329779cd272bef86411806c87
jhonnymunoz97 <jhonnyjmc97@gmail.com>
2021-09-02 05:24:13 -0500

    Finalizado el Backend

--- app/Http/Controllers/Api/V1/ProductsController.php
+++ app/Http/Controllers/Api/V1/ProductsController.php
@@ -26,8 +26,8 @@ class ProductsController extends Controller
         $image = (str_contains($image,'data:image/jpeg')) ? $image = str_replace( 'data:image/jpeg;base64,', '', $image) : $image = str_replace( 'data:image/png;base64,', '', $image);
         $image = str_replace(' ', '+', $image);
         $imageName = time() . '.' . 'png';
-        File::put(public_path() . "//imgProducts//" . $imageName, base64_decode($image));
-        $dir = asset("imgProducts/". $imageName);
+        File::put(public_path() . "//img//" . $imageName, base64_decode($image));
+        $dir = asset("img/". $imageName);
         return $dir;
     }
 
